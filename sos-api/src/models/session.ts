e93b41fed27a5ce83c5be0e05401887474c4cdb7
Haydar Ali <haydar.ali@devflovv.com>
2025-03-10 21:29:35 +0500

    added the feature of refresh token

--- sos-api/src/models/session.ts
+++ sos-api/src/models/session.ts
@@ -8,6 +8,7 @@ export class session extends Model {
   declare id: string;
   declare user_id: number;
   declare token: string;
+  declare refresh_token: string;
   static user_id: UUID;
 }
 
@@ -29,6 +30,10 @@ session.init(
       onDelete: 'CASCADE',
     },
     token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    refresh_token: {
       type: DataTypes.STRING,
       allowNull: false,
     },
--- sos-api/src/routes/user/auth.routes.ts
+++ sos-api/src/routes/user/auth.routes.ts
@@ -9,6 +9,8 @@ import {
   resetPassword,
   forgotPassword,
   googleAuthCallback,
+  refreshToken,
+  
 } from "../../controllers/user/auth.controller";
 import { userValidationSchemas } from "../../validation/user";
 import User from "../../models/user.model";
@@ -21,6 +23,7 @@ export default async function userRoutes(fastify: FastifyInstance) {
   fastify.route({ method: "POST", url: "/signup", schema: { body: joiToJsonSchema(userValidationSchemas.registerUserValidation) }, handler: createUserAccount, });
 
   fastify.route({ method: "POST", url: "/login", schema: { body: joiToJsonSchema(userValidationSchemas.loginUser) }, handler: loginUser, });
+  fastify.route({method: "POST", url: "/refresh-token", handler: refreshToken});
   fastify.route({ method: "POST", url: "/send-otp", handler: sendOtp });
   fastify.route({ method: "POST", url: "/user/verify-otp", handler: verifyOtp, });
   fastify.route({ method: "POST", url: "/user/forgot-password", handler: forgotPassword, });

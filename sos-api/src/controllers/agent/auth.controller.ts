2243512e74712b176b6bdc749aa6e46c56da3c1e
Haydar Ali <haydar.ali@devflovv.com>
2025-04-17 20:12:02 +0500

    change password api for agent

--- sos-api/src/controllers/agent/auth.controller.ts
+++ sos-api/src/controllers/agent/auth.controller.ts
@@ -9,15 +9,38 @@ import AuthUtils from '../../utils/authUtils';
 import { ref } from 'joi';
 import { AgentAuthService } from '../../services/auth/agent-auth.service';
 
-export const loginAgent = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
-) => {
-  const { email, password } = request.body as {
-    email: string;
-    password: string;
-  };
+class AgentAuthController {
+  public async loginAgent(request: FastifyRequest, reply: FastifyReply) {
+    const { email, password } = request.body as {
+      email: string;
+      password: string;
+    };
 
-  const authService = new AgentAuthService(email);
-  return await authService.login(email, password, reply);
-};
+    const authService = new AgentAuthService(email);
+    return await authService.login(email, password, reply);
+  }
+
+  public async changePassword(request: FastifyRequest, reply: FastifyReply) {
+    const { oldPassword, newPassword } = request.body as {
+      oldPassword: string;
+      newPassword: string;
+    };
+    try {
+      const user = await User.findByPk(request.user.id);
+      if (!user) {
+        return reply.status(404).send(errorResponse('User not found', 404));
+      }
+      const email = user.dataValues.email;
+      const authService = new AgentAuthService(email);
+      const passwordChanged = await authService.changePassword(email, oldPassword, newPassword);
+      if (passwordChanged) {
+        return reply.status(200).send(successResponse('Password changed successfully.', null, 200));
+      }
+    } catch (e) {
+      console.log(e);
+      return reply.status(500).send(errorResponse('Something went wrong', 500));
+    }
+  }
+}
+
+export default new AgentAuthController();
--- sos-api/src/middlewares/auth.middleware.ts
+++ sos-api/src/middlewares/auth.middleware.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import jwt from 'jsonwebtoken';
 import { errorResponse } from '../helper/responses';
 import { AuthStrategy } from './authStrategies';
+import { agent } from 'supertest';
 
 declare module 'fastify' {
   interface FastifyRequest {
@@ -28,6 +29,7 @@ export const authMiddleware = (strategy: AuthStrategy) => {
       const decoded = jwt.verify(token, JWT_SECRET) as {
         user_id: number;
         sos_user_id?: number;
+        agent_id?: number;
         role: string;
       };
 
@@ -36,27 +38,22 @@ export const authMiddleware = (strategy: AuthStrategy) => {
       }
 
       // **Check if the session exists**
-      const isSessionValid = await strategy.checkSession(
-        decoded.user_id,
-        token,
-      );
+      const isSessionValid = await strategy.checkSession(decoded.user_id, token);
       if (!isSessionValid) {
-        return reply
-          .status(401)
-          .send(errorResponse('Session not found or expired.', 401));
+        return reply.status(401).send(errorResponse('Session not found or expired.', 401));
       }
 
       // Attach user info to request
       request.user = {
         id: decoded.user_id,
         sos_user_id: decoded.sos_user_id,
+        agent_id: decoded.role === 'agent' ? decoded.agent_id : null,
         role: decoded.role,
       };
+      console.log('request.user :>> ', request.user);
     } catch (error) {
       console.error('Error in authMiddleware:', error);
-      return reply
-        .status(401)
-        .send(errorResponse('Invalid or expired token.', 401));
+      return reply.status(401).send(errorResponse('Invalid or expired token.', 401));
     }
   };
 };

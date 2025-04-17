d71d4c679271322d876582c75cc5f978cce6f9c2
Haydar Ali <haydar.ali@devflovv.com>
2025-04-17 22:22:40 +0500

    change password api for admin and its test

--- sos-api/src/controllers/admin/auth.controller.ts
+++ sos-api/src/controllers/admin/auth.controller.ts
@@ -8,15 +8,38 @@ import AuthUtils from '../../utils/authUtils';
 import { refreshToken } from '../user/auth.controller';
 import { AdminAuthService } from '../../services/auth/admin-auth.service';
 
-export const loginAdmin = async (
-  request: FastifyRequest,
-  reply: FastifyReply,
-) => {
-  const { email, password } = request.body as {
-    email: string;
-    password: string;
-  };
+class AdminAuthController {
+  public async loginAdmin(request: FastifyRequest, reply: FastifyReply) {
+    const { email, password } = request.body as {
+      email: string;
+      password: string;
+    };
 
-  const authService = new AdminAuthService(email);
-  return await authService.login(email, password, reply);
-};
+    const authService = new AdminAuthService(email);
+    return await authService.login(email, password, reply);
+  }
+
+  public async changePassword(request: FastifyRequest, reply: FastifyReply) {
+    const { old_password, new_password } = request.body as {
+      old_password: string;
+      new_password: string;
+    };
+    try {
+      const user = await User.findByPk(request.user.id);
+      if (!user) {
+        return reply.status(404).send(errorResponse('User not found', 404));
+      }
+      const email = user.dataValues.email;
+      const authService = new AdminAuthService(email);
+      const passwordChanged = await authService.changePassword(email, old_password, new_password);
+      if (passwordChanged) {
+        return reply.status(200).send(successResponse('Password changed successfully.', null, 200));
+      }
+    } catch (e) {
+      console.log(e);
+      return reply.status(500).send(errorResponse('Something went wrong', 500));
+    }
+  }
+}
+
+export default new AdminAuthController();
